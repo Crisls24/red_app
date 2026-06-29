c8b2bcbefdba2eaccd932f90070c7e41e16178d9
Cristopher <crisls@micorreo.upp.edu.mx>
2026-06-29 08:48:19 -0600

    Agregar soporte para imágenes y script de deploy para Render

--- database/migrations/2026_06_08_201132_create_images_table.php
+++ database/migrations/2026_06_08_201132_create_images_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->nullable();
-            $table->string('phone');
+            $table->string('url');
+            $table->morphs('imageable');
             $table->timestamps();
         });
+        
     }
 
     /**
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('images');
     }
 };
