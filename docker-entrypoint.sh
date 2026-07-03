0aafe6e23a11ca136684f5b530582cbb11c096fc
Cristopher <crisls@micorreo.upp.edu.mx>
2026-07-02 21:39:56 -0600

    Fix: crear .env desde .env.example si no existe en entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+
 if [ -z "$APP_KEY" ] || [ "$APP_KEY" = "base64:LPjudc04HobFqzOTvcALaoXeqI1WHoUfzTc4vm5SuVQ=" ]; then
     php artisan key:generate --force
 fi
