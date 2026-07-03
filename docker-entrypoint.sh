0c233eaaa108b23c74c7ccca92599b05c56af395
Cristopher <crisls@micorreo.upp.edu.mx>
2026-07-02 21:26:53 -0600

    Fix: configurar Apache para escuchar en puerto \ de Render

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,9 @@ fi
 php artisan migrate --force
 php artisan storage:link --force
 
+if [ -n "$PORT" ]; then
+    sed -i "s/Listen 80/Listen $PORT/" /etc/apache2/ports.conf
+    sed -i "s/*:80>/*:$PORT>/" /etc/apache2/sites-available/000-default.conf
+fi
+
 exec "$@"
